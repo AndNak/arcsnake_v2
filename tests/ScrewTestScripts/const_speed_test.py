85e2b36fd02641265abe5fb753460318a0adadd7
jlimkg <jkl009@ucsd.edu>
2022-09-07 17:23:33 -0700

    edited motor scripts

--- tests/ScrewTestScripts/const_speed_test.py
+++ tests/ScrewTestScripts/const_speed_test.py
@@ -12,23 +12,50 @@ arcsnake_v2_path = dirname(dirname(realpath(__file__)))
 sys.path.append(arcsnake_v2_path)  
 
 
+def get_time(t0):
+    return time.time() - t0
+
 if __name__ == "__main__":
     core.CANHelper.init("can0")
     can0 = can.ThreadSafeBus(channel='can0', bustype='socketcan')
 
     screwMotor = CanUJoint(can0, 1, 6, MIN_POS = 0 * 2 * 3.14, MAX_POS = 10 * 2 * 3.14)
     encoderMotor = CanUJoint(can0, 0, 1)
+
     sampling_rate = 200 # in Hz
+
+    ### Change these as needed
     run_time = 30 # in second
-    data_fname = '~/Documents/screw_test_data_files/motion_tests/set1/test1.csv'
+    set_num = 1
+    test_num = 1
     command_speed = -2.5 # in radians per second
+    data_fname = '~/Documents/screw_test_data_files/motion_tests/set{0}/test{1}.csv'.format(set_num, test_num)
 
     try:
         with open(data_fname, mode='w') as test_data:
             test_writer = csv.writer(test_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             test_writer.writerow(['time', 'angular speed', 'torque', 'linear speed'])
-            screwMotor.speed_ctrl(command_speed)
+
+            # synchronization procedure
+            t0 = time.time()
+
+            screwMotor.pos_ctrl(1.6, 6.0)
+            row = [get_time(t0), screwMotor.read_speed(), screwMotor.read_torque(
+            ), encoderMotor.read_speed()]
+            print(row)
+            test_writer.writerow(row)
+            time.sleep(2)
+
+            screwMotor.pos_ctrl(0, 6.0)
+            row = [get_time(t0), screwMotor.read_speed(), screwMotor.read_torque(
+            ), encoderMotor.read_speed()]
+            print(row)
+            test_writer.writerow(row)
+
+            time.sleep(15)
+
             start_time = time.time()
+            screwMotor.speed_ctrl(command_speed)
             while True:
                 cur_time = time.time() - start_time
                 row = [cur_time, screwMotor.read_speed(), screwMotor.read_torque(), encoderMotor.read_speed()]
@@ -40,7 +67,7 @@ if __name__ == "__main__":
     except(KeyboardInterrupt) as e:
         print(e)
 
-    screwMotor.motor_stop() 
+    screwMotor.motor_stop()
     encoderMotor.motor_stop()
 
     print('Done')
--- tests/ScrewTestScripts/double_screw_test.py
+++ tests/ScrewTestScripts/double_screw_test.py
@@ -20,20 +20,22 @@ if __name__ == "__main__":
     screwMotor2 = CanUJoint(can0, 2, 5, MIN_POS = 0 * 2 * 3.14, MAX_POS = 10 * 2 * 3.14)
     encoderMotor = CanUJoint(can0, 0, 1)
     sampling_rate = 200 # in Hz
-    run_time = 30 # in second
-    data_fname = '~/Documents/screw_test_data_files/motion_tests/set1/test1.csv'
+    run_time = 10 # in second
+    set_num = 1
+    test_num = 1
+    data_fname = '~/Documents/screw_test_data_files/motion_tests/set{0}/test{1}.csv'.format(set_num, test_num)
     command_speed = -2.5 # in radians per second
 
     try:
         with open(data_fname, mode='w') as test_data:
             test_writer = csv.writer(test_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            test_writer.writerow(['time', 'angular speed', 'torque', 'linear speed'])
+            test_writer.writerow(['time', 'angular speed 1', 'angular speed 2', 'torque 1', 'torque 2', 'linear speed'])
             screwMotor1.speed_ctrl(command_speed)
             screwMotor2.speed_ctrl(command_speed)
             start_time = time.time()
             while True:
                 cur_time = time.time() - start_time
-                row = [cur_time, screwMotor1.read_speed(), screwMotor1.read_torque(), encoderMotor.read_speed()]
+                row = [cur_time, screwMotor1.read_speed(), screwMotor2.read_speed(), screwMotor1.read_torque(), screwMotor2.read_torque(), encoderMotor.read_speed()]
                 print(row)
                 test_writer.writerow(row)
                 time.sleep(1/sampling_rate)
@@ -43,6 +45,7 @@ if __name__ == "__main__":
         print(e)
 
     screwMotor1.motor_stop() 
+    screwMotor2.motor_stop()
     encoderMotor.motor_stop()
 
     print('Done')
