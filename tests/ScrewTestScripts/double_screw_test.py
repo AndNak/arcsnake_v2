6d0c861621470dbd09323f55a49b98be2f8f529e
Mew463 <mwyeoh123@gmail.com>
2022-09-15 11:36:57 -0700

    testing

--- tests/ScrewTestScripts/double_screw_test.py
+++ tests/ScrewTestScripts/double_screw_test.py
@@ -26,33 +26,31 @@ if __name__ == "__main__":
     encoderMotor = CanUJoint(can0, 2, 1)
     sampling_rate = 200 # in Hz
 
-    run_time = 10 # in second
+    run_time = 30 # in second
     set_num = 1
-    test_num = 4
-    command_speed = 2.0 # in radians per second
+    test_num = 5
+    command_speed = 6.0 # in radians per second
     data_fname = 'tests/ScrewTestScripts/data_files/doublescrew_tests/set{0}/test{1}.csv'.format(set_num, test_num)
 
     try:
-        time.sleep(3)
+        t0 = time.time()
         with open(data_fname, mode='w') as test_data:
             test_writer = csv.writer(test_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            test_writer.writerow(['time', 'angular speed 1', 'angular speed 2', 'torque 1', 'torque 2', 'linear speed', 'angpos'])
+            test_writer.writerow(['time', 'angular speed 1', 'angular speed 2', 'torque 1', 'torque 2', 'linear speed', 'linear position (meters)'])
 
             # # synchronization procedure
-            screwMotor1.pos_ctrl(0, 3.0)
-            screwMotor2.pos_ctrl(0, 3.0)
-            row = [get_time(t0), screwMotor1.read_speed(), screwMotor2.read_speed(), screwMotor1.read_torque(), screwMotor2.read_torque(), encoderMotor.read_speed()]
-            print(row)
-            test_writer.writerow(row)
-            time.sleep(30)
+            screwMotor1.pos_ctrl(0, 5.0)
+            screwMotor2.pos_ctrl(0, 5.0)
+
+            input('Press Enter to start trial')
 
             ### Start main trial loop
             t1 = time.time()
             screwMotor1.speed_ctrl(-command_speed)
             screwMotor2.speed_ctrl(command_speed)
             while True:
-                row = [get_time(t0), screwMotor1.read_speed(), screwMotor2.read_speed(), screwMotor1.read_torque(), screwMotor2.read_torque(), encoderMotor.read_speed()]
-                print(row)
+                row = [get_time(t0), screwMotor1.read_speed(), screwMotor2.read_speed(), screwMotor1.read_torque(), screwMotor2.read_torque(), encoderMotor.read_speed(), encoderMotor.read_multiturn_position() *-.09525/2]
+                print(row[6])
                 test_writer.writerow(row)
                 time.sleep(1/sampling_rate)
                 if get_time(t1) > run_time:
@@ -67,3 +65,4 @@ if __name__ == "__main__":
     print('Done')
 
     core.CANHelper.cleanup("can0")
+ 
